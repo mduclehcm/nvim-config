ede079abb8b007c05b4406bf6f8ad4de6ba96408
Duc Le <minhducle.dev@gmail.com>
2026-01-04 15:11:53 +0700

    fix misstyping keymap

--- lua/ducle/core/keymaps.lua
+++ lua/ducle/core/keymaps.lua
@@ -1,9 +1,11 @@
 local opts = { noremap = true, silent = true }
 
-vim.gmaplocalleader = " "
+vim.g.maplocalleader = " "
 vim.g.mapleader = " "
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "moves lines down in visual selection" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "moves lines up in visual selection" })
 
 vim.keymap.set("n", "J", "mzJ`z")
+
+
