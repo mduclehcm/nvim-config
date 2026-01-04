4e65a2d3a242b24b7eb6a35d021993da24f26c30
Duc Le <minhducle.dev@gmail.com>
2026-01-04 15:34:35 +0700

    add more keymap

--- lua/ducle/core/keymaps.lua
+++ lua/ducle/core/keymaps.lua
@@ -8,4 +8,30 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "moves lines up in visual 
 
 vim.keymap.set("n", "J", "mzJ`z")
 
+vim.keymap.set("v", "<", "<gv", opts)
+vim.keymap.set("v", ">", ">gv", opts)
+
+-- Clipboard
+-- Paste without replace clipboard content
+vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set("v", "p", '"_dp', opts)
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+vim.keymap.set("i", "<C-c>", "<Esc>")
+vim.keymap.set("n", "<C-c>", ":nohl<CR>", { desc = "Clear search hl", silent = true })
+
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+vim.keymap.set("n", "Q", "<nop>")
+
+-- tab
+vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>")    -- open new tab
+vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>")  -- close new tab
+vim.keymap.set("n", "<leader>tn", "<cmd>tabn<CR>")      -- goto next tab
+vim.keymap.set("n", "<leader>tp", "<cmd>tabp<CR>")      -- goto prev tab
+vim.keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>")  -- open current file in new tab
+
+-- split
+vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
+vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make split equal size" })
+vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
 
